7a7982aa84baa1869e637777e363e7c0610943af
omer117 <omer.asraf91@gmail.com>
2022-09-04 17:24:56 +0300

    fixxx & folders fixing

--- src/Components/CardComponent/ProductCardComponent.js
+++ src/Components/CardComponent/ProductCardComponent.js
@@ -6,43 +6,49 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import "./ProductCardComponent.scss"
 import Grid from '@mui/material/Grid';
-import { Link,useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 
 function ProductCardComponent(props) {
 
     let { catagory } = useParams();
 
-    
+
 
     console.log(catagory);
     return (
         <>
-        <Grid item xs={12} sm={6} md={4}>
-        <Link to={`/shop/${catagory}/${props.data.id}`}>
-            <Card className="product-card">
-                <CardMedia
-                className="product-image"
-                component="img"
-                height="199"
-                    image={props.data.image}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
-                        {props.data.title}
-                    </Typography>
-                    <Typography>
-                        {props.data.price}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button>
-                        Add to Cart
-                    </Button>
-                </CardActions>
-            </Card>
+            <Grid item xs={12} sm={6} md={4}>
+                <Link to={`/shop/${catagory}/${props.data.id}`}>
+                    <Card className="product-card">
+                        <CardMedia
+                            className="product-image"
+                            component="img"
+                            height="199"
+                            image={props.data.image}
+                        />
+                        <CardContent>
+                            <Typography
+                                gutterBottom
+                                variant="subtitle1"
+                                className="product-title"
+                                component="div">
+                                {props.data.title}
+                            </Typography>
+                            <Typography
+                                className="product-price"
+                                variant="body1">
+                                {`$${props.data.price}`}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button>
+                                Add to Cart
+                            </Button>
+                        </CardActions>
+                    </Card>
                 </Link>
-                </Grid>
+            </Grid>
         </>
     )
 }
