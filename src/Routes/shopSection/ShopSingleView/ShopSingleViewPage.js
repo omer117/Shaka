8d3ba539786b8bbea6d52fd1bbd69ffd1da40ae1
omer117 <omer.asraf91@gmail.com>
2022-09-06 10:52:33 +0300

    added single view func

--- src/Routes/shopSection/ShopSingleView/ShopSingleViewPage.js
+++ src/Routes/shopSection/ShopSingleView/ShopSingleViewPage.js
@@ -1,30 +1,37 @@
 import { useParams } from "react-router-dom"
 import axios from "axios"
 import { useEffect, useState } from "react";
+import "./ShopSingleViewPage.scss"
 
 
 function ShopSingleViewPage(props) {
-    let [product,setProducts] = useState({})
+    let [product, setProducts] = useState({})
     const catagory = useParams()
 
-    
-console.log(catagory)
 
-    useEffect(() => {        
+    console.log(catagory)
+
+    useEffect(() => {
         axios.post('/getProduct',
             [catagory.id, catagory.catagory]
         ).then((response) => {
             setProducts(JSON.parse(response.data));
-            // console.log(response);
         }).then((err) => {
             console.log(err);
         })
     }, [])
-    
+
     console.log(product);
     return (
-        <div>
-        </div>
+        <>
+            <div className="mainContainer">
+                <img className="productImage" src={product.image} />
+                <div className="SingleViewContainer">
+                    sdfg
+                </div>
+
+            </div>
+        </>
     )
 }
 
