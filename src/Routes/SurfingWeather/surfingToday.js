ce8d725b94b4ee7f70ba59bd4ed14d6a1821ef19
omer117 <omer.asraf91@gmail.com>
2022-09-22 14:03:40 +0300

    added surfing today functinality

--- src/Routes/SurfingWeather/surfingToday.js
+++ src/Routes/SurfingWeather/surfingToday.js
@@ -1,10 +1,92 @@
-
+import axios from "axios"
+import { useEffect, useState } from "react"
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import AirIcon from '@mui/icons-material/Air';
+import SurfingIcon from '@mui/icons-material/Surfing';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
 
 function SurfingTodayComponent() {
+    const [beaches, setBeaches] = useState([])
+    const [selectedBeach, setSelectedBeach] = useState(1)
+    const [selectedBeachDetails, setSelectedBeachDetails] = useState({})
+
+    const handleChange = (event) => {
+        setSelectedBeach(event.target.value);
+    };
+
+
+    useEffect(() => {
+        axios.get(`/getBeaches`)
+            .then((res) => setBeaches(res.data))
+            .catch((err) => console.log(err))
+    }, [])
+
+
+    useEffect(() => {
+        axios.post('/everyDayGet',
+            { sqlString: `SELECT * FROM daily_forecast WHERE beach_id=${selectedBeach}` })
+            .then((res) => setSelectedBeachDetails(res.data))
+            .catch((err) => console.log(err));
+    }, [selectedBeach])
+
+    console.log(selectedBeachDetails);
+
+
+    const beachesList = beaches.map((beach) => {
+        return (<MenuItem key={beach.beach_id} value={beach.beach_id}>{beach.beach_name}</MenuItem>)
+    })
+
+    function WeatherDiv() {
+        if(selectedBeachDetails.length > 0){
+
+            return (<div className="surfingTodayDiv">
+            <h2>The nearest beach - {selectedBeachDetails[0].beach_name} </h2>
+            <div className="beachDetails">
+                <div>
+                    <h4>wind Speed</h4>
+                    <AirIcon className="detail" />
+                    <p>{selectedBeachDetails[0].wind_speed}</p>
+                </div>
+                <div>
+                    <h4>wave height</h4>
+                    <SurfingIcon className="detail" />
+                    <p>{selectedBeachDetails[0].wave_height}</p>
+                </div>
+                <div>
+                    <h4>water temperature</h4>
+                    <ThermostatIcon className="detail" />
+                    <p>{selectedBeachDetails[0].water_temperature}</p>
+                </div>
+            </div>
+        </div>)
+        }
+    }
+
 
     return (
         <>
-            <div>asdsfasdfasdfasdfasdfasdfasdf</div>
+            <div className="mainDiv">
+
+                <div className="form">
+
+                    <FormControl>
+                        <InputLabel id="form">beach </InputLabel>
+                        <Select
+                            id="formDataSelector"
+                            value={selectedBeach}
+                            label="beach"
+                            autoWidth
+                            onChange={handleChange}
+                        >
+                            {beachesList}
+                        </Select>
+                    </FormControl>
+                </div>
+                {selectedBeachDetails === undefined ? <div>loading...</div> : <WeatherDiv details={selectedBeachDetails[0]}/>}
+            </div>
         </>
     )
 }
